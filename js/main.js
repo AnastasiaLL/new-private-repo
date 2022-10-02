fd8a0e011a0b72d06e3131102389c59073f418a9
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-10-02 20:47:39 +0300

    refactor: change sort function

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-let users = "http://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
-let corsApi = "https://cors-anywhere.herokuapp.com/";
+const users = 'http://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture';
+const corsApi = 'https://cors-anywhere.herokuapp.com/';
 let objData;
 const peopleContent = {
-  avatarUser: document.querySelectorAll(".img-medium"),
-  titleUser: document.querySelectorAll(".peoples__title")
+  avatarUser: document.querySelectorAll('.img-medium'),
+  titleUser: document.querySelectorAll('.peoples__title'),
 };
 const peoplePopup = {
-  mainPop: document.querySelector(".peoples-popup"),
-  imagesLarge: document.querySelector(".img-large"),
-  title: document.querySelector(".popup-title"),
-  street: document.querySelector(".people-street"),
-  city: document.querySelector(".people-city"),
-  state: document.querySelector(".people-state"),
-  email: document.querySelector(".people-email"),
-  phone: document.querySelector(".people-telephone")
+  mainPop: document.querySelector('.peoples-popup'),
+  imagesLarge: document.querySelector('.img-large'),
+  title: document.querySelector('.popup-title'),
+  street: document.querySelector('.people-street'),
+  city: document.querySelector('.people-city'),
+  state: document.querySelector('.people-state'),
+  email: document.querySelector('.people-email'),
+  phone: document.querySelector('.people-telephone'),
 };
 
 function load(callback, api) {
   fetch(corsApi + api)
-    .then(function (response) {
+    .then((response) => {
       console.log(response);
       return Promise.all([response.status, response.json()]);
     })
-    .then(function (result) {
-      if (result[0] != 200) {
+    .then((result) => {
+      if (result[0] !== 200) {
         console.log(`Error: ${result[0]}`);
       } else {
         callback(result[1]);
         console.log(result[1]);
       }
     })
-    .catch(function (error) {
-      alert(`Error: ${error}`);
+    .catch((error) => {
+      if (error.name === 'TypeError') {
+        alert(`Error: ${error.name}. Please, unlock access to the demo on https://cors-anywhere.herokuapp.com/corsdemo`);
+      } else alert(`Error: ${error.name}`);
     });
 }
 
@@ -40,56 +42,47 @@ function transferData(data) {
   render(objData);
 }
 
-function render(objData) {
-  for (let i = 0; i < objData.length; i++) {
-    let userAvatar = objData[i]["picture"]["medium"];
-    let userTitle = objData[i]["name"]["title"]; 
-    let userFirstName = objData[i]["name"]["first"];
-    let userLastName = objData[i]["name"]["last"];
+function render(renderedObject) {
+  for (let i = 0; i < renderedObject.length; i += 1) {
+    const userAvatar = renderedObject[i].picture.medium;
+    const userTitle = renderedObject[i].name.title;
+    const userFirstName = renderedObject[i].name.first;
+    const userLastName = renderedObject[i].name.last;
 
     peopleContent.avatarUser[i].src = userAvatar;
     peopleContent.titleUser[i].textContent = `${userTitle}. ${userFirstName} ${userLastName}`;
   }
 }
 
-function alphabetSort() {
-  let mode = 1;
-  generalSort (mode);
-  render(objData);
-}
-
-function backSort() {
-  let mode = -1;
-  generalSort (mode);
-  render(objData);
-}
-
-function generalSort (mode){
-  objData.sort(function (firstItem, secondItem) {
-    if (firstItem.name.last > secondItem.name.last) return mode;
-    if (firstItem.name.last < secondItem.name.last) return -mode;
-    if (firstItem.name.last == secondItem.name.last) return 0;
-  });
-}
-
 function listenToTheEvent() {
-  document.getElementById("sortAlphabet").addEventListener("click", alphabetSort);
-  document.getElementById("backSort").addEventListener("click", backSort);
-
-  document.querySelector(".close").addEventListener("click", ()=> {
-    togglePopup(false)
+  document.getElementById('sortAlphabet').addEventListener('click', () => {
+    dataSort(1);
   });
-  
-  let peoples = document.querySelectorAll(".peoples");
-  for (let i = 0; i < peoples.length; i++) {
-    peoples[i].addEventListener( "click", ()=>{
-      renderPopup(objData);
-    })
+  document.getElementById('backSort').addEventListener('click', () => {
+    dataSort(-1);
+  });
+
+  document.querySelector('.close').addEventListener('click', () => {
+    togglePopup(false);
+  });
+
+  const peoples = document.querySelectorAll('.peoples');
+  for (let i = 0; i < peoples.length; i += 1) {
+    peoples[i].addEventListener('click', renderPopup);
   }
 }
 
-function renderPopup(obj) {
-  const person = obj[event.currentTarget.dataset.index - 1]
+function dataSort(mode) {
+  objData.sort((firstItem, secondItem) => {
+    if (firstItem.name.last > secondItem.name.last) return mode;
+    if (firstItem.name.last < secondItem.name.last) return -mode;
+    return 0;
+  });
+  render(objData);
+}
+
+function renderPopup() {
+  const person = objData[window.event.currentTarget.dataset.index - 1];
 
   peoplePopup.title.innerHTML = `Full name: ${person.name.title}. ${person.name.first} ${person.name.last}`;
   peoplePopup.city.innerHTML = `City: ${person.location.city}`;
@@ -99,19 +92,18 @@ function renderPopup(obj) {
   peoplePopup.phone.innerHTML = `Phone: ${person.phone}`;
   peoplePopup.imagesLarge.src = person.picture.large;
 
-  togglePopup(true)
+  togglePopup(true);
 }
 
 function togglePopup(flag) {
   let displayStyle;
-  if (flag){
-    displayStyle = "display:block"
-  }else {
-    displayStyle = "display:none"
+  if (flag) {
+    displayStyle = 'display:block';
+  } else {
+    displayStyle = 'display:none';
   }
-  peoplePopup.mainPop.setAttribute("style", displayStyle);
+  peoplePopup.mainPop.setAttribute('style', displayStyle);
 }
-
 
 listenToTheEvent();
 load(transferData, users);
